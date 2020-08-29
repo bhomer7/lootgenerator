8e599755b522fe324e88e80e7c791058be0f48f4
Ben Homer <ben.homer@smartobjx.com>
2020-08-28 22:51:14 -0500

    Replace tabs with spaces

--- lootgenerator/generator.py
+++ lootgenerator/generator.py
@@ -3,20 +3,20 @@ import json
 
 
 def load_table(table_name):
-	with open('./tables/' + table_name) as file:
-		table = json.load(file)
-	return table
+    with open('./tables/' + table_name) as file:
+        table = json.load(file)
+    return table
 
 
 def print_loot(loot):
-	pass
+    pass
 
 
 def main():
-	table = Generator.load_table('TreasureHoard0to4.json')
-	loot = Generator.roll_loot(table)
-	Generator.print_loot(loot)
+    table = Generator.load_table('TreasureHoard0to4.json')
+    loot = Generator.roll_loot(table)
+    Generator.print_loot(loot)
 
 
 if __name__=='__main__':
-	main()
+    main()
